b573f7c31470f89d7bd5b2638e98f675b7798342
EnesCeran <cceran52@gmail.com>
2024-11-06 14:47:24 +0300

    Login system plan.

---
+++ app/src/main/java/com/x_force/unimar/login/AuthHandler.java
@@ -0,0 +1,14 @@
+package com.x_force.unimar.login;
+public class AuthHandler {
+
+    public AuthHandler()
+    {
+        //FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    }
+
+    public void RegisterUser()
+    {
+        //FirebaseAuth.getInstance().createUserWithEmailAndPassword()
+    }
+
+}
