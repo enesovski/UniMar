a1a3c04fac48ddade503f4967c1ab609d6eb8ee7
EnesCeran <cceran52@gmail.com>
2024-11-10 20:44:45 +0300

    Login system implemented. (However, activity_login.xml does not show, needs to  be fixed)

--- app/src/main/java/com/x_force/unimar/login/AuthHandler.java
+++ app/src/main/java/com/x_force/unimar/login/AuthHandler.java
@@ -1,14 +1,38 @@
 package com.x_force.unimar.login;
+
+import com.google.firebase.auth.FirebaseAuth;
+
 public class AuthHandler {
+    private final FirebaseAuth mAuth;
 
-    public AuthHandler()
-    {
-        //FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    public AuthHandler() {
+        this.mAuth = FirebaseAuth.getInstance();
     }
 
-    public void RegisterUser()
-    {
-        //FirebaseAuth.getInstance().createUserWithEmailAndPassword()
+    public void loginUser(String email, String password, IAuthCallback callback) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        callback.onSuccess("Login successful!");
+                    } else {
+                        callback.onFailure("Authentication failed.");
+                    }
+                });
     }
 
+    public void registerUser(String email, String password, IAuthCallback callback) {
+        if (password.length() < 6) {
+            callback.onFailure("Password must be at least 6 characters.");
+            return;
+        }
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        callback.onSuccess("Registration successful!");
+                    } else {
+                        callback.onFailure("Registration failed.");
+                    }
+                });
+    }
 }
