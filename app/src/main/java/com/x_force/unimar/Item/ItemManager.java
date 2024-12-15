0c7ea3b641172cf60a145baf06466ec4224cafaf
Deniz Sönmez <denizsonikk@outlook.com>
2024-12-15 12:35:04 +0300

    ProductListingCallback implemented successfully

--- app/src/main/java/com/x_force/unimar/Item/ItemManager.java
+++ app/src/main/java/com/x_force/unimar/Item/ItemManager.java
@@ -11,22 +11,32 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class ItemManager{
+public class ItemManager {
     static FirebaseFirestore db = ProductListingActivity.getDb();
     static Query productQuery = db.collection("productListing");
     static Query tutoringQuery = db.collection("tutorListing");
-    static ItemAdapter adapter = ProductListingActivity.getAdapter();
+    public static ItemAdapter adapter;
+    static ProductListCallback callback = new ProductListCallback() {
+        public void onProductListLoaded(List<Item> items) {
+            adapter.items = items;
+            adapter.notifyDataSetChanged();
+        }
+
+        @Override
+        public void onProductListError(Exception e) {
+
+        }
+    };
 
     public static void addToList(Item item) {
-        if(item instanceof Product) {
+        if (item instanceof Product) {
             db.collection("productListing").add(item).addOnSuccessListener(documentReference -> {
                 item.setDocId(documentReference.getId());
                 Log.d("Firestore", "Document added with Id: " + item.getDocId()); // sysout
             }).addOnFailureListener(e -> {
                 Log.w("Firestore", "Error adding document!", e); //sysout
             });
-        }
-        else if(item instanceof Tutoring){
+        } else if (item instanceof Tutoring) {
             db.collection("tutorListing").add(item).addOnSuccessListener(documentReference -> {
                 item.setDocId(documentReference.getId());
                 Log.d("Firestore", "Document added with Id: " + item.getDocId());
@@ -35,106 +45,110 @@ public class ItemManager{
             });
         }
     }
-    public static void deleteFromList(Item item){
-        if(item instanceof Product) {
+
+    public static void deleteFromList(Item item) {
+        if (item instanceof Product) {
             db.collection("productListing").document(item.getDocId()).delete().addOnSuccessListener(documentReference -> {
                 Log.d("Firestore", "Document deleted with Id: " + item.getDocId());
             }).addOnFailureListener(e -> {
                 Log.w("Firestore", "Error deleting document", e);
             });
-        }
-        else if(item instanceof Tutoring) {
+        } else if (item instanceof Tutoring) {
             db.collection("tutorListing").document(item.getDocId()).delete().addOnSuccessListener(documentReference -> {
-                Log.d("Firestore", "Document deleted with Id: " + item.getDocId());
-            }
+                        Log.d("Firestore", "Document deleted with Id: " + item.getDocId());
+                    }
             ).addOnFailureListener(e -> {
                 Log.w("Firestore", "Error deleting document", e);
             });
         }
     }
+
     public static List<Item> sortProductList(char c) { // eğer c 'A' karakteri ise artan fiyata göre sıralıyor. D--> azalan. İkisi de değilse sortlanmamış databaseyi yazdırıyor
         ArrayList<Item> items = new ArrayList<>();
         c = Character.toUpperCase(c);
-        if(c == 'A'){
+        if (c == 'A') {
             productQuery = productQuery.orderBy("cost", Query.Direction.ASCENDING);
-        }
-        else if(c == 'D'){
+        } else if (c == 'D') {
             productQuery = productQuery.orderBy("cost", Query.Direction.DESCENDING);
-        }
-        else {
+        } else {
 
         }
-        productQuery.get().addOnCompleteListener(done ->{
-            if(done.isSuccessful()){
-                for(QueryDocumentSnapshot document : done.getResult()){
+        productQuery.get().addOnCompleteListener(done -> {
+            if (done.isSuccessful()) {
+                for (QueryDocumentSnapshot document : done.getResult()) {
                     Product product = document.toObject(Product.class);
                     items.add(product);
                     Log.d("Firestore", document.getId() + " => " + document.getData());
                 }
-            }else{
+                callback.onProductListLoaded(items);
+            } else {
                 Log.w("Firestore", "Error getting documents: ", done.getException());
             }
         });
 
         return items;
     }
+
     public static List<Item> sortTutorList(char c) {
         List<Item> items = Collections.emptyList();
         c = Character.toUpperCase(c);
         Query query;
-        if(c == 'A'){
+        if (c == 'A') {
             query = db.collection("tutorListing").orderBy("cost", Query.Direction.ASCENDING);
-        }
-        else if(c == 'D'){
+        } else if (c == 'D') {
             query = db.collection("tutorListing").orderBy("cost", Query.Direction.DESCENDING);
-        }
-        else {
+        } else {
             query = db.collection("tutorListing");
         }
-        query.get().addOnCompleteListener(done ->{
-            if(done.isSuccessful()){
-                for(QueryDocumentSnapshot document : done.getResult()){
+        query.get().addOnCompleteListener(done -> {
+            if (done.isSuccessful()) {
+                for (QueryDocumentSnapshot document : done.getResult()) {
                     Product product = document.toObject(Product.class);
                     items.add(product);
                     Log.d("Firestore", document.getId() + " => " + document.getData());
                 }
-            }else{
+            } else {
                 Log.w("Firestore", "Error getting documents: ", done.getException());
             }
         });
 
         return items;
     }
-    public static List<Item> filterProductList(double min, double max){
+
+    public static List<Item> filterProductList(double min, double max) {
         List<Item> items = Collections.emptyList();
-        productQuery = productQuery.whereGreaterThanOrEqualTo("cost",min);
+        productQuery = productQuery.whereGreaterThanOrEqualTo("cost", min);
 
         productQuery = productQuery.whereLessThanOrEqualTo("cost", max);
 
-        productQuery.get().addOnCompleteListener(done ->{
-            if(done.isSuccessful()){
-                for(QueryDocumentSnapshot document : done.getResult()){
+        productQuery.get().addOnCompleteListener(done -> {
+            if (done.isSuccessful()) {
+                for (QueryDocumentSnapshot document : done.getResult()) {
                     Product product = document.toObject(Product.class);
                     items.add(product);
                     Log.d("Firestore", document.getId() + " => " + document.getData());
                 }
-            }else{
+            } else {
                 Log.w("Firestore", "Error getting documents: ", done.getException());
             }
         });
 
         return items;
     }
-    public static Query getProductQuery(){
+
+    public static Query getProductQuery() {
         return productQuery;
     }
-    public static void refreshProductQuery(){
+
+    public static void refreshProductQuery() {
         productQuery = db.collection("productListing");
     }
-    public static Query getTutorQuery(){
+
+    public static Query getTutorQuery() {
         return tutoringQuery;
     }
-    public static void refreshTutorQuery(){
+
+    public static void refreshTutorQuery() {
         tutoringQuery = db.collection("tutorListing");
     }
 }
