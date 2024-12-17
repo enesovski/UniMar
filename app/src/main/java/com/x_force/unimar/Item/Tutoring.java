ac62355b55aca4db5764b2dc282a4419698375b9
Deniz Sönmez <denizsonikk@outlook.com>
2024-12-17 21:37:47 +0300

    UserId and UserName is stored in each product now

--- app/src/main/java/com/x_force/unimar/Item/Tutoring.java
+++ app/src/main/java/com/x_force/unimar/Item/Tutoring.java
@@ -1,14 +1,5 @@
 package com.x_force.unimar.Item;
 
-import android.util.Log;
-
-import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.Query;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
-import com.x_force.unimar.MainActivity;
-
 import java.util.ArrayList;
 
 public class Tutoring extends Item{
