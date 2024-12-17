4de3b5ce5ea9851f2e1414daa3cca4ed82c83bd0
Yavuz Selim Kahyaoğlu <yavuzskhy@gmail.com>
2024-12-17 20:22:55 +0300

    Product Photo Implementation Product image added.

--- app/src/main/java/com/x_force/unimar/Item/ItemAdapter.java
+++ app/src/main/java/com/x_force/unimar/Item/ItemAdapter.java
@@ -2,11 +2,17 @@ package com.x_force.unimar.Item;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.util.Base64;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.SearchView;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
@@ -14,9 +20,12 @@ import androidx.cardview.widget.CardView;
 
 import com.x_force.unimar.R;
 import com.x_force.unimar.Views.ProductView;
+
 import java.util.List;
+
 //Listede gözükecek her bir item için nasıl görüneceğini
 public class ItemAdapter extends ArrayAdapter<Item> {
+
     List<Item> items;
     public ItemAdapter(Context context, List<Item> items){
         super(context,0,items);
@@ -28,6 +37,7 @@ public class ItemAdapter extends ArrayAdapter<Item> {
     // Listedeki her item için ui'a göre bir element oluşturuyoruz.
     public View getView(int position, View convertView, ViewGroup parent) {
         Item item = items.get(position);
+
         if(convertView == null){
             convertView = LayoutInflater.from(getContext())
                     .inflate(R.layout.activity_productlisting, parent, false);
@@ -40,6 +50,14 @@ public class ItemAdapter extends ArrayAdapter<Item> {
         TextView descTextView = convertView.findViewById(R.id.item_desc);
         View gradientBar = convertView.findViewById(R.id.gradientBar);
         CardView card = convertView.findViewById(R.id.card);
+        ImageView imageView = convertView.findViewById(R.id.item_image);
+
+        if(item.getClass().equals(Product.class) && ((Product) item).getImage() != null){
+            Bitmap imageBitmap= decodeBase64toBitmap(((Product) item).getImage());
+            imageView.setImageBitmap(imageBitmap);
+        }
+
+
 
         nameTextView.setText(item.getName());
         costTextView.setText(String.valueOf(item.getCost()));
@@ -48,6 +66,7 @@ public class ItemAdapter extends ArrayAdapter<Item> {
         TextView sortButton = convertView.findViewById(R.id.sort_button);
         TextView filterButton = convertView.findViewById(R.id.filter_button);
         SearchView searchBar = convertView.findViewById(R.id.product_searchbar);
+
 
         addNewButton.setVisibility(View.GONE);
         viewButton.setVisibility(View.VISIBLE);
@@ -68,9 +87,18 @@ public class ItemAdapter extends ArrayAdapter<Item> {
             intent.putExtra("Category", categories);
             intent.putExtra("Description", item.getDesc());
             intent.putExtra("Cost", item.getCost()+"");
+            if(item instanceof Product){
+                intent.putExtra("Image", ((Product) item).getImage());
+            }
             getContext().startActivity(intent);
         });
 
         return convertView;
+    }
+    public static Bitmap decodeBase64toBitmap(String image) {
+        byte[] decodedBytes = Base64.decode(image, Base64.DEFAULT);
+        Bitmap decodedBitmap = BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes.length);
+
+        return decodedBitmap;
     }
 }
--- app/src/main/java/com/x_force/unimar/ItemAddActivity.java
+++ app/src/main/java/com/x_force/unimar/ItemAddActivity.java
@@ -1,10 +1,14 @@
 package com.x_force.unimar;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Base64;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -15,9 +19,16 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+
 public class ItemAddActivity extends AppCompatActivity {
 
     private ActivityResultLauncher<Intent> imagePickerLauncher;
+
+    private Uri productImage;
+    private String image;
+    ImageView addPhotoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,13 +42,20 @@ public class ItemAddActivity extends AppCompatActivity {
             return insets;
         });
 
+        addPhotoView = findViewById(R.id.add_picture_icon);
         // Initialize the ActivityResultLauncher
         imagePickerLauncher = registerForActivityResult(
                 new ActivityResultContracts.StartActivityForResult(),
                 result -> {
                     if (result.getResultCode() == RESULT_OK && result.getData() != null) {
-                        Uri selectedImageUri = result.getData().getData();
-                        // TODO: Use the selectedImageUri to display the selected image or upload it.
+                        productImage = result.getData().getData();
+                        addPhotoView.setImageURI(productImage);
+                        try {
+                            image= convertImageToBase64(productImage);
+                        } catch (Exception e) {
+                            throw new RuntimeException(e);
+                        }
+
                     }
                 }
         );
@@ -52,7 +70,10 @@ public class ItemAddActivity extends AppCompatActivity {
         EditText newItemDescText = findViewById(R.id.new_item_desc_text);
         EditText newItemCostText = findViewById(R.id.new_item_cost_text);
 
-        uploadPhotoButton.setOnClickListener(v -> openImagePicker());
+        uploadPhotoButton.setOnClickListener(v -> {
+            openImagePicker();
+
+        });
 
         chooseCategoryButton.setOnClickListener(e -> {
             if(newItemNameText.getText().length() != 0
@@ -63,6 +84,7 @@ public class ItemAddActivity extends AppCompatActivity {
                 intent.putExtra("name",newItemNameText.getText().toString());
                 intent.putExtra("desc",newItemDescText.getText().toString());
                 intent.putExtra("cost",newItemCostText.getText().toString());
+                intent.putExtra("image",image);
                 startActivity(intent);
                 finish();
             } else {
@@ -75,5 +97,14 @@ public class ItemAddActivity extends AppCompatActivity {
         Intent intent = new Intent(Intent.ACTION_PICK);
         intent.setType("image/*");
         imagePickerLauncher.launch(intent);
+    }
+    private String convertImageToBase64(Uri imageUri) throws Exception {
+        InputStream inputStream = getContentResolver().openInputStream(imageUri);
+        Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 50, byteArrayOutputStream); // Compress to 50%
+        byte[] imageBytes = byteArrayOutputStream.toByteArray();
+        return Base64.encodeToString(imageBytes, Base64.DEFAULT);
     }
 }
